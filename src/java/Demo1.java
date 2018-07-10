c5afc147d9eafe43b075e4f31aa12228bd50a16d
Jangrant <953982108@qq.com>
2018-07-10 11:41:00 +0800

    change com

--- src/java/Demo1.java
+++ src/java/Demo1.java
@@ -1,7 +1,7 @@
 public class Demo1 {
     public static void main(String[] args){
         System.out.println("this is a change!!");
-        System.out.println("this line is add.");
+        System.out.println("this line is kjahskjd.");
 
     }
 }
