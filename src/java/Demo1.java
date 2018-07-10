0e9cb950271930a623fe705f2528c4dc25a0a32a
Jangrant <953982108@qq.com>
2018-07-10 11:12:06 +0800

    change test

--- src/java/Demo1.java
+++ src/java/Demo1.java
@@ -1,5 +1,7 @@
 public class Demo1 {
     public static void main(String[] args){
-        System.out.println("this is a test!!");
+        System.out.println("this is a change!!");
+        System.out.println("this line is add.");
+
     }
 }
